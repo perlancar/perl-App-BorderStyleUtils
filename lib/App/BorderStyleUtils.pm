c321984c6331b17fcbd40d1614477438bb9548bc
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-01-23 08:42:43 +0700

    Add CLI: show-border-style

--- lib/App/BorderStyleUtils.pm
+++ lib/App/BorderStyleUtils.pm
@@ -40,6 +40,83 @@ sub list_border_style_modules {
     [200, "OK", \@res, \%resmeta];
 }
 
+$SPEC{show_border_style} = {
+    v => 1.1,
+    summary => 'Show example table with specified border style',
+    args => {
+        style => {
+            schema => 'perl::borderstyle::modname_with_optional_args*',
+            req => 1,
+            pos => 0,
+        },
+    },
+};
+sub show_border_style {
+    require Module::Load::Util;
+
+    my %args = @_;
+
+    my @res;
+    my %resmeta;
+
+    my $bs = Module::Load::Util::instantiate_class_with_optional_args(
+        {ns_prefix=>'BorderStyle'}, $args{style});
+
+    return [412, "Box char styles not supported yet"]
+        if $bs->get_struct->{box_chars};
+
+    my $map = {
+        A => sub { $bs->get_border_char(0, 0) },
+        B => sub { $bs->get_border_char(0, 1) },
+        C => sub { $bs->get_border_char(0, 2) },
+        D => sub { $bs->get_border_char(0, 3) },
+        E => sub { $bs->get_border_char(1, 0) },
+        F => sub { $bs->get_border_char(1, 1) },
+        G => sub { $bs->get_border_char(1, 2) },
+        H => sub { $bs->get_border_char(2, 0) },
+        I => sub { $bs->get_border_char(2, 1) },
+        J => sub { $bs->get_border_char(2, 2) },
+        K => sub { $bs->get_border_char(2, 3) },
+        a => sub { $bs->get_border_char(2, 4) },
+        b => sub { $bs->get_border_char(2, 5) },
+        L => sub { $bs->get_border_char(3, 0) },
+        M => sub { $bs->get_border_char(3, 1) },
+        N => sub { $bs->get_border_char(3, 2) },
+        O => sub { $bs->get_border_char(4, 0) },
+        P => sub { $bs->get_border_char(4, 1) },
+        Q => sub { $bs->get_border_char(4, 2) },
+        R => sub { $bs->get_border_char(4, 3) },
+        e => sub { $bs->get_border_char(4, 4) },
+        f => sub { $bs->get_border_char(4, 5) },
+        g => sub { $bs->get_border_char(4, 6) },
+        h => sub { $bs->get_border_char(4, 7) },
+        S => sub { $bs->get_border_char(5, 0) },
+        T => sub { $bs->get_border_char(5, 1) },
+        U => sub { $bs->get_border_char(5, 2) },
+        V => sub { $bs->get_border_char(5, 3) },
+    };
+
+    my $table = <<'_';
+ABBBCBBBCBBBCBBBD
+E       F   F   G
+HIIIaIIIJIIIbIIIK
+L   M   M       N
+OPPPfPPPQPPPePPPR
+L       M   M   N
+OPPPPPPPQPPPePPPR
+L       M       N
+L       gPPPPPPPR
+L       M       N
+OPPPPPPPh       N
+L       M       N
+STTTTTTTUTTTTTTTV
+_
+
+    $table =~ s{(.)}{$map->{$1} ? $map->{$1}->() : $1}eg;
+
+    [200, "OK", $table];
+}
+
 1;
 # ABSTRACT: CLI utilities related to border styles
 
